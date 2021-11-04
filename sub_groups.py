e72c0797d1924b90765bca6e1e5c0511a0cd3176
Shayswisa7 <70646828+Shayswisa7@users.noreply.github.com>
2021-11-04 03:13:19 +0200

    Add files via upload
    Code shortcut. Download unnecessary commands

--- sub_groups.py
+++ sub_groups.py
@@ -3,24 +3,17 @@ def v(arr,arr1,a,a1,b,b1,c,c1):
     if a//b!=2 or  a1//b1!=1:
         if a//b > a1//b1+1:
             v(arr, arr1, a, a1+b1, b, b1, c, c1)
-        elif a//b == a1//b1+1:
-            v(arr, arr1, a1, a1+b1*c, b*c, b1*c, c, c1) 
 def f(arr1,a):
-    if isinstance(arr1[0], int):
-        return a
     if a<=10:
-        return arr1[a%10-1]
+        return str(arr1[a%10-1])
     return f(arr1,a//10)+f'{arr1[a%10-1]}'
-
 def fun(arr,arr1,a,a1,b,b1,c,c1):
     arr1.append(f(arr,a))
     if c1 < c:
         if a//a1>=2:
             v(arr, arr1, a, a+b1, a1, b1, a1, b)
         fun(arr, arr1, a+1, a1, b, b1, c, c1+1)
-    elif c!=0:
-        if a//a1>=2:
-            v(arr, arr1, a, a+b1, a1, b1, a1, b)
+    elif c>0:
         fun(arr, arr1, a+b, a1, b+1,b1, c-1, 0)
     else:
         if a//a1>1:
@@ -35,7 +28,7 @@ def add1(arr3,arr1,l,i,i1,j,j1):
         
 def main():
     #     0   1   2   3   4   5   6   7   8    9   10   11   12
-    arr=['~','!','@','#','$','%','^','&','*', '(',')' ,'_' , '+']
+    arr=[1,2,3,4,5,6,7,8,9]#'~','!','@','#','~','!','@','#'
     arr1=[]
     arr3=[]
     if len(arr)>3:
